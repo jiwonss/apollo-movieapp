af0c9c323051e02077cd6410f915e581f514258f
jiwonss <sjs3326@naver.com>
2021-05-12 18:09:33 +0900

    local state

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,11 +6,13 @@ import styled from "styled-components";
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
+      id
       title
       medium_cover_image
       language
       rating
       description_intro
+      isLiked @client
     }
     suggestions(id: $id) {
       id
@@ -63,14 +65,17 @@ export default () => {
   return (
     <Container>
       <Column>
-        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        <Title>
+          {loading
+            ? "Loading..."
+            : `${data.movie.title} ${data.movie.isLiked ? "❤" : "😢"}`}
+        </Title>
         <Subtitle>
           {data?.movie?.language} · {data?.movie?.rating}
         </Subtitle>
         <Description>{data?.movie?.description_intro}</Description>
       </Column>
-      <Poster bg={data?.data.movie?.medium_cover_image}></Poster>
-      {data && data.suggestions && data}
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
     </Container>
   );
 };
